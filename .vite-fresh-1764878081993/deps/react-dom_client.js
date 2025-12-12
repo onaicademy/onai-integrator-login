7d521535a22575d0dc680cf9074116e019b7827a
onAI Academy <smmmcwin@gmail.com>
2025-12-12 22:19:02 +0500

    chore: Remove Vite cache artifacts from git, add .vite-fresh-* to gitignore
    - Removed 372 JavaScript files from .vite-fresh-* temporary cache folders
    - These are Vite build artifacts that should not be tracked
    - Added .vite-fresh-* pattern to .gitignore
    - Real codebase: 100% TypeScript (441 .ts/.tsx files)
    - This will fix GitHub language stats showing 90% JavaScript

--- .vite-fresh-1764878081993/deps/react-dom_client.js
+++
@@ -1,8 +0,0 @@
-import {
-  require_client
-} from "./chunk-DO4JO5NU.js";
-import "./chunk-NEZJQHI7.js";
-import "./chunk-FXJVXTVJ.js";
-import "./chunk-4B2QHNJT.js";
-export default require_client();
-//# sourceMappingURL=react-dom_client.js.map
